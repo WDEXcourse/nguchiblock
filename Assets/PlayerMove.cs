6412b9b28bb9d14db8a2bf990d56bc00b6a8b6ce
WDEXcourse <wdkawasaki.online@gmail.com>
2023-02-10 19:16:35 +0900

    スタート座標を修正

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -8,7 +8,7 @@ public class PlayerMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(0, 1, 0);
     }
 
     // Update is called once per frame
