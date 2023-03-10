285b63f71f46e0ed27afaa003f0cf37e28a7b5fd
WDEXcourse <wdkawasaki.online@gmail.com>
2023-03-10 16:25:19 +0900

    3/10 ジャンプ機能を追加

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public Rigidbody rb;
+    public float jumpPower;
+    private bool isJumping = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,28 +17,51 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 前に移動
         if (Input.GetKey(KeyCode.UpArrow))
         {
             rb.AddForce(0, 0, 5);
         }
 
+        //　後ろに移動
         if (Input.GetKey(KeyCode.DownArrow))
         {
             rb.AddForce(0, 0, -5);
         }
 
+        //　右に移動
         if (Input.GetKey(KeyCode.RightArrow))
         {
             rb.AddForce(5, 0, 0);
         }
+
+        // 左に移動
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb.AddForce(-5, 0, 0);
         }
 
+        // 落ちたらスタート位置に戻る
         if (transform.position.y < -10)
         {
             transform.position = new Vector3(0, 8, 0);
+        }
+
+        // ジャンプ
+        if (Input.GetKey(KeyCode.Space) && !isJumping)
+        {
+            rb.velocity = Vector3.up * jumpPower;
+            isJumping = true;
+        }
+    }
+
+    // 地面にいるかどうかの判定
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            print("着地");
+            isJumping = false;
         }
     }
 }
--- Assets/TrapMove.cs
+++ Assets/TrapMove.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrapaMove : MonoBehaviour
+public class TrapMove : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
